331360f3caf165d54512e3385734ca57f891c8aa
Dylan <dylan.reeder5@gmail.com>
2022-05-18 14:24:34 -0400

    exploring mutable vars

---
+++ variables/target/debug/deps/variables-c1c865ac158f1a46.d
@@ -0,0 +1,5 @@
+/Users/dylan/Documents/Learning/rust/variables/target/debug/deps/variables-c1c865ac158f1a46.rmeta: src/main.rs
+
+/Users/dylan/Documents/Learning/rust/variables/target/debug/deps/variables-c1c865ac158f1a46.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/deps/variables-e6c10e7d108d2dda.d
@@ -0,0 +1,5 @@
+/Users/dylan/Documents/Learning/rust/variables/target/debug/deps/variables-e6c10e7d108d2dda.rmeta: src/main.rs
+
+/Users/dylan/Documents/Learning/rust/variables/target/debug/deps/variables-e6c10e7d108d2dda.d: src/main.rs
+
+src/main.rs:
