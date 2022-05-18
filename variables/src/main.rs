6314b166641ac95f49fdb955970c8bc6a5b363de
Dylan <dylan.reeder5@gmail.com>
2022-05-18 14:30:35 -0400

    exploring variable shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,18 @@
 fn main() {
-    let mut x = 5;
-    println!("the value of x is: {}", x);
-    x = 6;
-    println!("the value of x is: {}", x);
+    /* The other difference between mut and shadowing is that because we’re effectively creating a new variable when we use the let keyword again, we can change the type of the value but reuse the same name. */
+    let x = 5;
+
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("the val of x in the inner scope is: {}", x);
+    }
+    println!("the val of x is : {}", x);
+
+    /* saves time and readability to not have to create separate variables such as spaces_str and spaces_num
+
+    i.e.
+        let spaces = "   ";
+        let spaces = spaces.len();
+    */
 }
