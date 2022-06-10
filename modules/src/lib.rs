fc0ada0b69e88f24b7765dfe146fa2cb7c014f95
Dylan <dylan.reeder5@gmail.com>
2022-06-09 20:43:42 -0400

    little more of modules

--- modules/src/lib.rs
+++ modules/src/lib.rs
@@ -1,13 +1,20 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+pub mod back_of_house {
+    pub struct Breakfast {
+       pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit:String::from("peaches"),
+            }
+        }
     }
 }
 
 pub fn eat_at_resturant() {
-    // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // relative
-    front_of_house::hosting::add_to_waitlist();
+   let mut meal = back_of_house::Breakfast::summer("beans");
+   meal.toast = String::from("Wheat");
 }
