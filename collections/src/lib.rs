cf8f532c377427a96541cf77419d5feb0780551a
Dylan <dylan.reeder5@gmail.com>
2022-06-11 17:24:32 -0400

    exploring hash maps

--- collections/src/lib.rs
+++ collections/src/lib.rs
@@ -1,20 +1,38 @@
-fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    // let v2 = vec![1,2,3]; 
+// fn cool_vecs() {
+//     let v: Vec<i32> = Vec::new();
+//      let v2 = vec![1,2,3];
+//
+//      v.push(1);
+//      v.push(2);
+//      v.push(3);
+//
+//     let third: &i32 = &v[2];
+//     println!("an element is {}", third);
+//
+//     match v.get(2) {
+//         Some(third) => println!("an element is {}", third),
+//         None => println!("Nope."),
+//     };
+//
+//     for i in &v {
+//         println!("{}", i)
+//     }
+// }
 
-    v.push(1);
-    v.push(2);
-    v.push(3);
+use std::collections::HashMap;
 
-    let third: &i32 = &v[2];
-    println!("an element is {}", third);
+fn cool_maps() {
+    let mut scores = HashMap::new();
 
-    match v.get(2) {
-        Some(third) => println!("an element is {}", third),
-        None => println!("Nope."),
-    }
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 
-    for i in &v {
-        println!("{}", i)
-    }
+    // let teams = vec![String::from("Blue"), String::from("Yellow")];
+    // let initial_scores = vec![10, 50];
+    //
+    // let mut scores: HashMap<_, _> =
+    //     teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
 }
