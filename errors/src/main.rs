166c75c484d31713e494c3ac74edd7d92266bcd0
Dylan <dylan.reeder5@gmail.com>
2022-06-12 10:12:14 -0400

    unwrap is cool

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,6 +1,25 @@
-fn main() {
-    // When the panic! macro executes, your program will print a failure message, unwind and clean up the stack, and then quit.
-    let so_big_of_a_vec = vec![1,2,3];
+use std::{fs::File, io::ErrorKind};
 
-    let _v = so_big_of_a_vec[99];
+fn main() {
+
+    // If the Result value is the Ok variant, unwrap will return the value inside the Ok. 
+    // If the Result is the Err variant, unwrap will call the panic! macro for us.
+    let f = File::open("hello.txt").unwrap();
+
+    // works similarly to unwrap() but expect uses a manual error message
+    // let f = File::open("hello.txt").expect("a good error message");
+
+    // let _f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         // looking for a file that doesn't exist
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem Creating file: {:?}", e),
+    //         },
+    //         other_err => {
+    //             panic!("Problem opening file: {:?}", other_err)
+    //         }
+    //     },
+    // };
 }
